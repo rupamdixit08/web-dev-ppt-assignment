599d3694ad8f472ea2e2045498924367eb2ef8ad
rupamdixit08 <90315216+rupamdixit08@users.noreply.github.com>
2023-07-15 14:13:23 +0530

    Add files via upload

--- Assignment-7/Github_user_finder/script.js
+++ Assignment-7/Github_user_finder/script.js
@@ -1,17 +1,45 @@
 function getUserData() {
-    const username = document.getElementById('username').value;
-  
-    // Make a GET request to the GitHub API
-    fetch(`https://api.github.com/users/${username}`)
-      .then(response => response.json())
-      .then(data => {
-        // Update the user information
-        document.getElementById('avatar').src = data.avatar_url;
-        document.getElementById('name').textContent = data.name;
-      })
-      .catch(error => {
-        console.log('Error:', error);
-        alert('An error occurred. Please try again.');
-      });
-  }
-  
+  const username = document.getElementById("usernameInput").value;
+  const apiUrl = `https://api.github.com/users/${username}`;
+
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      if (data.message === "Not Found") {
+        // User not found
+        displayErrorMessage("User not found.");
+      } else {
+        displayUserData(data);
+      }
+    })
+    .catch(error => {
+      displayErrorMessage("An error occurred while fetching user data.");
+      console.error(error);
+    });
+}
+
+function displayErrorMessage(message) {
+  document.getElementById("userContainer").classList.add("hidden");
+  alert(message);
+}
+
+function displayUserData(user) {
+  const userContainer = document.getElementById("userContainer");
+  const avatar = document.getElementById("avatar");
+  const name = document.getElementById("name");
+  const portfolio = document.getElementById("portfolio");
+  const location = document.getElementById("location");
+  const publicRepos = document.getElementById("publicRepos");
+  const followers = document.getElementById("followers");
+  const bio = document.getElementById("bio");
+
+  avatar.src = user.avatar_url;
+  name.textContent = user.name || "Name not provided";
+  portfolio.textContent = user.blog || "Portfolio not provided";
+  location.textContent = user.location || "Location not provided";
+  publicRepos.textContent = `Public Repos: ${user.public_repos}`;
+  followers.textContent = `Followers: ${user.followers}`;
+  bio.textContent = user.bio || "Bio not provided";
+
+  userContainer.classList.remove("hidden");
+}
